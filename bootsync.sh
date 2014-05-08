f34d1478d4eae7f4f8b3b4146414f0f318239d75
rastating <rob@rastating.com>
2014-05-08 23:31:46 +0100

    Added bootsync script for auto start-up

---
+++ bootsync.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# put other system startup commands here, the boot process will wait until they$
+# Use bootlocal.sh for system startup commands that can run in the background
+# and therefore not slow down the boot process.
+/usr/bin/sethostname box
+/opt/bootlocal.sh &
+
+python /home/tc/keycore.py
